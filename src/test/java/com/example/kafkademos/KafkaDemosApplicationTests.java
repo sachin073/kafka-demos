686ab994738bc5065362864f293bd5eda24484e2
sachin <verma.sachin@outlook.in>
2022-09-25 16:09:10 +0530

    init kafka repo

---
+++ src/test/java/com/example/kafkademos/KafkaDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkademos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
